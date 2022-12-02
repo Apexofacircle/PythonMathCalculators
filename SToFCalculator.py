8fe1b2be71910294ba25b7fd5979bc88084b29ed
Ada <96547482+Apexofacircle@users.noreply.github.com>
2022-12-02 08:24:53 -0500

    Version 1.0
    Standard to factored form calculator

---
+++ SToFCalculator.py
@@ -0,0 +1,37 @@
+#Credit goes to https://github.com/Apexofacircle 
+print("Standard to factored form calcualtor")
+print("Version: 1.0")
+print("Asuming: Ax^2 + Bx + C")
+
+#Get inputs
+_A = float(input("A: "))
+_B = float(input("B: "))
+_C = float(input("C: "))
+ 
+#Set starting variables
+_Div = 1
+_Ac = _A *_C
+ 
+#Calculate
+if _Ac >= _Div: #If in the negatives 
+        _Div = -1
+        while not (_Ac / _Div) + _Div == _B :
+            _Div -= 1
+            print (_Div, _Ac / _Div, _B)
+else: #If in the positives 
+    while not (_Div / _Ac ) + _Div == _B :
+        _Div += 1
+        print (_Div, _Div / _Ac , _B)
+ 
+# Turn calculation into readable format
+_DivAns = _Div / _A
+try:
+    _Ans2 = (_Ac / _Div) / _A
+except: 
+    _Ans2 = 0
+ 
+ 
+#Print answer
+print("-----")
+print("Answer: ")
+print(f"{_A}(x + {_DivAns})(x + {_Ans2})")
